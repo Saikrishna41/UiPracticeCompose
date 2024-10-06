ca738ea6a3d96b8088f752fbf08be2312861d78e
Saikrishna41 <saai.krrishna@gmail.com>
2024-10-06 13:24:25 +0530

    added hyperlink in compose

--- app/src/main/java/com/task/uipracticecompose/MainActivity.kt
+++ app/src/main/java/com/task/uipracticecompose/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import com.task.uipracticecompose.ui.theme.UiPracticeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UiPracticeComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +37,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen(modifier: Modifier = Modifier) {
+    HyperLinkText(
+        modifier = modifier.padding(top = 56.dp, start = 10.dp, end = 10.dp),
+        fullText = "By signing in you agree to the privacy policy and terms of use",
+        linkText = listOf("privacy policy", "terms of use"),
+        hyperLinks = listOf("https://www.fb.com", "https://www.google.com")
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UiPracticeComposeTheme {
-        Greeting("Android")
+fun HyperLinkText(
+    modifier: Modifier = Modifier,
+    fullText: String,
+    linkText: List<String>,
+    linkTextColor: Color = Color.Blue,
+    linkTextFontWeight: FontWeight = FontWeight.Medium,
+    linkTextDecoration: TextDecoration = TextDecoration.Underline,
+    hyperLinks: List<String>,
+    fontSize: TextUnit = TextUnit.Unspecified
+) {
+    val annotatedString = buildAnnotatedString {
+        append(fullText)
+        linkText.forEachIndexed { index, link ->
+            val startIndex = fullText.indexOf(link)
+            val endIndex = startIndex + link.length
+            addStyle(
+                style = SpanStyle(
+                    color = linkTextColor,
+                    fontSize = fontSize,
+                    fontWeight = linkTextFontWeight,
+                    textDecoration = linkTextDecoration
+                ),
+                start = startIndex,
+                end = endIndex
+            )
+            addStringAnnotation(
+                tag = "URL",
+                annotation = hyperLinks[index],
+                start = startIndex,
+                end = endIndex
+            )
+
+            addStyle(
+                style = SpanStyle(
+                    fontSize = fontSize
+
+                ),
+                start = 0,
+                end = fullText.length
+            )
+        }
+    }
+
+    val uriHandler = LocalUriHandler.current
+    ClickableText(
+        modifier = modifier,
+        text = annotatedString
+    ) {
+        annotatedString.getStringAnnotations("URL", it, it).firstOrNull()?.let { stringAnnotation ->
+            uriHandler.openUri(stringAnnotation.item)
+        }
     }
 }
